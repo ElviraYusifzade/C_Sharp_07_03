b182969072133d6ffd1374711ff60fe409da3eac
Elvira Yusifzade <elya200909@mail.ru>
2023-03-07 22:38:47 +0300

    Task 1_3

---
+++ Lesson_1/1_3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("N=");
+int n=int.Parse(Console.ReadLine()!);
+
+
+int count=-n;
+
+while (count!=n)
+{
+        Console.WriteLine(count);
+    if (n<0)
+{
+    count --;
+}
+  else
+  {
+    count ++;
+  }
+}
