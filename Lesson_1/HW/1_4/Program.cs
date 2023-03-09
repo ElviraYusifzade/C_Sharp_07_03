9a104f07f255c2abcc56c43f442a44e13c9f544d
Elvira Yusifzade <elya200909@mail.ru>
2023-03-09 19:58:43 +0300

    Task 4

---
+++ Lesson_1/HW/1_4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Enter the number");
+
+int number = int.Parse(Console.ReadLine()!);
+
+int evenNumber = 2;
+
+if(number > 1)
+{
+    while(evenNumber <= number)
+    {
+        Console.WriteLine(evenNumber + " ");
+        evenNumber = evenNumber + 2;
+    }
+}
