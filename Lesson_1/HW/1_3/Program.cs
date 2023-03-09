6ec8bfbceffbb72c80101cb0d3ca7107cc11f09d
Elvira Yusifzade <elya200909@mail.ru>
2023-03-09 19:50:39 +0300

    Task 3

---
+++ Lesson_1/HW/1_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число
+// и выдаёт, является ли число чётным (делится ли оно на два, без остатка).
+
+Console.WriteLine("Enter the number");
+
+int number = int.Parse(Console.ReadLine()!);
+
+int remDiv = number % 2;
+
+if(remDiv == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
