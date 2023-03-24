96ffc6b38d5b21db848db654231fe7fbe4abf31f
Elvira Yusifzade <elya200909@mail.ru>
2023-03-24 20:24:14 +0300

    HW Task 4_1

---
+++ Lesson_4/HW/4_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Enter a number");
+int i = int.Parse(Console.ReadLine()!);
+int sum = 0;
+
+while (i > 0)
+{
+int num = i % 10;
+i = i / 10;
+sum = sum + num;
+}
+Console.WriteLine("the sum of all digits in the number is equal to: " + sum);
