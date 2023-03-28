065d647db91c35bb1a21ae66db6f11fc98783411
Elvira Yusifzade <elya200909@mail.ru>
2023-03-28 20:07:04 +0300

    Task 7_3

--- Lesson_7/7_3/Program.cs
+++ Lesson_7/7_3/Program.cs
@@ -1,9 +1,6 @@
 ﻿// Задайте двумерный массив.
 // Найдите сумму элементов главной диагонали.
 
-// https://4apple.org/summa-diagonalnyh-jelementov-matricy/
-// https://ru.wikipedia.org/wiki/%D0%93%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D0%B4%D0%B8%D0%B0%D0%B3%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C
-
 Console.Write("Кол-во элементов в строке:");
 int y = int.Parse(Console.ReadLine()!);
 Console.Write("Кол-во элементов в столбце:");
@@ -44,7 +41,6 @@ void PrintArray(int[,] arr)
 
 int[,] array = MakeArray(x, y, a, b);
 PrintArray(array);
-////////////////////////////////////////////
 
 int Sum(int[,] arr)
 {
