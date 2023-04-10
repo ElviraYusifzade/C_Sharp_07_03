c2068e83e69e4e0715d601e310bfd3fc97d3517f
Elvira Yusifzade <elya200909@mail.ru>
2023-04-10 15:00:48 +0300

    Task 9_0

---
+++ Lesson_9/9_0/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N при помощи рекурсии.
+
+Console.Write("Enter the first number: ");
+int N = int.Parse(Console.ReadLine()!);
+
+void PrintNums(int n)
+{
+    if (n == 0) return;
+    PrintNums(n - 1);
+    Console.Write($"{n} ");
+}
+
+Console.Write("Result: ");
+PrintNums(N);
